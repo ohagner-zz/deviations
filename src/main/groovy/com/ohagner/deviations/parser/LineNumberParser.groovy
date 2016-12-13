dac5db828965e9ac4f55ccc32c9da8936172248f
Olle Hagner <olov.hagner@enfo.se>
2016-12-13 16:46:54 +0100

    Made lineparsing more robust

--- src/main/groovy/com/ohagner/deviations/parser/LineNumberParser.groovy
+++ src/main/groovy/com/ohagner/deviations/parser/LineNumberParser.groovy
@@ -2,15 +2,17 @@ package com.ohagner.deviations.parser
 
 class LineNumberParser {
 
-    final static String PATTERN = /[\w\s]*\s((?:\d*[\,\s]?)+)/
+    final static String PATTERN = /[A-Öa-ö\s]*\s((?:[\dA-Öa-ö]*[\,\s]?)+)/
 
     List<String> extractLineNumbers(String scopeElements) {
         def parts = scopeElements.split(";")
         def lineNumbers = []
         parts.each { part ->
             def matcher=(part =~ PATTERN)
-            def foundNumbers = matcher[0][1]
-            lineNumbers.addAll(foundNumbers.split(",").collect { it.trim() })
+            if(matcher.matches()) {
+                def foundNumbers = matcher[0][1]
+                lineNumbers.addAll(foundNumbers.split(",").collect { it.trim() })
+            }
         }
 
         return lineNumbers
